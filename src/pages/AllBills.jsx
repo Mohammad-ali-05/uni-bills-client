48c65aacce68029bedf232a9d2639d7929630f86
Mohammad Ali <muhammadali.pass1@gmail.com>
2025-11-30 11:35:03 +0600

    Added filter functionality and filtering from server side

--- src/pages/AllBills.jsx
+++ src/pages/AllBills.jsx
@@ -4,19 +4,47 @@ import BillsCard from "../components/BillsCard";
 
 const AllBills = () => {
   const [billsData, setBillsData] = useState();
-  console.log(billsData);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000/bills")
+    fetch(`http://localhost:3000/bills${selectedCategory ? `?category=${selectedCategory}` : ""}`)
       .then((res) => res.json())
       .then((data) => setBillsData(data))
       .catch((error) => console.log(error.code, error.message));
-  }, []);
+  }, [selectedCategory]);
+
+  console.log(billsData);
+  console.log(selectedCategory);
+
+  const handleCategory = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
   return (
     <section className="max-w-[1440px] w-full  mx-auto my-20 p-2 lg:px-10">
       <div className="my-10">
         <h2 className="font-semibold text-5xl text-center">All bills</h2>
+      </div>
+      <div className="flex justify-between items-center my-2.5">
+        <div></div>
+        <div>
+          <label
+            htmlFor="category"
+            className="block mb-1 font-medium text-gray-700">
+            Filter by Category:
+          </label>
+          <select
+            id="category"
+            onChange={handleCategory}
+            value={selectedCategory}
+            className="border border-gray-300 rounded p-2">
+            <option value="">All Categories</option>
+            <option value="Electricity">Electricity</option>
+            <option value="Gas">Gas</option>
+            <option value="Water">Water</option>
+            <option value="Internet">Internet</option>
+          </select>
+        </div>
       </div>
       <div className="grid grid-cols-3 bg-gray-300 rounded-xl gap-5 py-10 px-5">
         {billsData?.map((billData, index) => (
